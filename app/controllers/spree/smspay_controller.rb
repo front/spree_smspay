559f49f8247bb378777859102ef3dd7b0f7650b8
Segi Henggana <henggana7@gmail.com>
2015-08-03 12:43:55 +0700

    Remove smspay_controller#confirm method

--- app/controllers/spree/smspay_controller.rb
+++ app/controllers/spree/smspay_controller.rb
@@ -1,37 +1,5 @@
 module Spree
   class SmspayController < StoreController
-    def confirm
-      order = current_order || raise(ActiveRecord::RecordNotFound)
-
-      begin
-        if provider.login
-          order.payments.create!(
-            source: SmspayCheckout.create(
-              phone_code: params[:phone_code],
-              phone_number: params[:phone_number],
-              order: order),
-            amount: order.total,
-            payment_method: payment_method
-          )
-          order.next
-          if order.complete?
-            flash.notice = Spree.t(:order_processed_successfully)
-            flash[:order_completed] = true
-            session[:order_id] = nil
-            redirect_to completion_route(order)
-          else
-            redirect_to checkout_state_path(order.state)
-          end
-        else
-          flash[:error] = Spree.t('flash.generic_error', scope: 'smspay', reasons: "Unauthorized")
-          redirect_to checkout_state_path(order.state)
-        end
-      rescue SocketError
-        flash[:error] = Spree.t('flash.connection_failed', scope: 'smspay')
-        redirect_to checkout_state_path(:payment)
-      end
-    end
-
     def success
       if params[:status].blank? || params[:reference].blank? || params[:invoice].blank?
         render json: "Reference, Status and Invoice fields must be present", status: 400
